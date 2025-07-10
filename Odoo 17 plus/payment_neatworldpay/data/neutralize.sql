7f59f6adbdbe293d00dd18acebb5e2bf2cf59c1f
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-10 16:13:48 +0300

    Init commit 16

--- Odoo 17 plus/payment_neatworldpay/data/neutralize.sql
+++
@@ -1,7 +0,0 @@
-
-UPDATE payment_provider
-   SET neatworldpay_entity = NULL,
-       neatworldpay_connection_url = NULL,
-       neatworldpay_use_iframe = NULL,
-       neatworldpay_username = NULL,
-       neatworldpay_password = NULL;
